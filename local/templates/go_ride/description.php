e8a10b66250addd3e35743bd71e03bf3822f93b2
aurogonnn <artembulatov450@gmail.com>
2025-11-18 14:55:06 +0000

    Интеграция шаблона

---
+++ local/templates/go_ride/description.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arTemplate = array(
+    "NAME" => "GO&RIDE",
+    "DESCRIPTION" => "Шаблон сайта для дипломного проекта",
+    "SORT" => 100,
+    "TYPE" => "",
+);
+?>
