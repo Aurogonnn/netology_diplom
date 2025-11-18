678c14bf2e8fdb18c94aac902d543fdc32bac51f
aurogonnn <artembulatov450@gmail.com>
2025-11-18 14:06:38 +0000

    the first push files

---
+++ .section.php
@@ -0,0 +1,7 @@
+<?php
+$sSectionName = "Главная страница";
+$arDirProperties = [
+	'description' => 'Магазин велосипедов',
+	'keywords' => 'велосипед, педали, колеса, спорт, отдых',
+	'robots' => 'index, follow',
+];
